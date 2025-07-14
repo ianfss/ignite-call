257207637a55316841554da02a88325b6fad29b2
Ian <ssianfilipe@gmail.com>
2025-07-14 18:16:29 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { ThemeProvider } from '@/components/theme-provider'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Ignite Call',
+  description: 'A simple online scheduling system',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
