da577d3c754de406345c0e84219d3cd8aa83d99c
Ian <ssianfilipe@gmail.com>
2025-07-17 19:34:28 -0300

    refactor: merge register forms into pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,51 @@
-import { ClaimUsernameForm } from '@/components/claim-username-form'
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Deve ter no mínimo 3 caracteres.' })
+    .regex(/^[a-z-]+$/i, { message: 'Deve ter apenas letras e hifens.' })
+    .refine((username) => !username.startsWith('-'), {
+      message: 'Não pode começar com hífen.',
+    })
+    .refine((username) => !username.endsWith('-'), {
+      message: 'Não pode terminar com hífen.',
+    })
+    .transform((username) => username.toLowerCase()),
+})
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  })
+
+  const router = useRouter()
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { username } = values
+
+    await router.push(`/register?username=${username}`)
+  }
+
   return (
     <div className="mx-auto mt-20 max-w-[572px] py-4">
       <div className="mx-auto max-w-[480px]">
@@ -9,7 +54,37 @@ export default function Home() {
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </p>
-        <ClaimUsernameForm />
+        <Card>
+          <CardContent>
+            <Form {...form}>
+              <form
+                className="grid gap-2 md:grid-cols-[1fr_auto]"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          placeholder="seu-usuario"
+                          prefix="ianfss.com/"
+                          {...field}
+                          disabled={form.formState.isSubmitting}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button disabled={form.formState.isSubmitting} type="submit">
+                  Reservar <ArrowRight />
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,89 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import z from 'zod'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { Multistep } from '@/components/ui/multistep'
-import { RegisterForm } from './register-form'
+
+const whitespaceRegex = /\s+/
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Deve ter no mínimo 3 caracteres.' })
+    .regex(/^[a-z-]+$/i, { message: 'Deve ter apenas letras e hifens.' })
+    .refine((username) => !username.startsWith('-'), {
+      message: 'Não pode começar com hífen.',
+    })
+    .refine((username) => !username.endsWith('-'), {
+      message: 'Não pode terminar com hífen.',
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'Nome deve ter pelo menos 3 caracteres.' })
+    .refine((val) => val.trim().split(whitespaceRegex).length >= 2, {
+      message: 'Informe nome e sobrenome.',
+    }),
+})
 
 export default function Register() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      name: '',
+    },
+  })
+
+  const params = useSearchParams()
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const username = params.get('username')
+
+    if (username) {
+      form.setValue('username', username)
+    }
+  }, [params, form.setValue])
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: values.username,
+        name: values.name,
+      }),
+    })
+
+    if (response.ok) {
+      await router.push('/register/connect-calendar')
+    } else {
+      const { message } = await response.json()
+
+      toast.error(message)
+    }
+  }
+
   return (
     <div className="mx-auto mt-20 max-w-[572px] py-4">
       <div className="space-y-6 px-6">
@@ -12,7 +94,54 @@ export default function Register() {
         </p>
         <Multistep currentStep={1} size={4} />
       </div>
-      <RegisterForm />
+      <Card className="mt-6">
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome de usuário</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={form.formState.isSubmitting}
+                        placeholder="seu-usuario"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={form.formState.isSubmitting}
+                        placeholder="Seu nome"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button disabled={form.formState.isSubmitting} type="submit">
+                Próximo passo <ArrowRight />
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   )
 }
