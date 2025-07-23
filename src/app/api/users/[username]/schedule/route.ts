728942ae280218a5e8e9901b9b2e9296210d4b5e
Ian <ssianfilipe@gmail.com>
2025-07-23 09:05:37 -0300

    feat(scheduling): create event on google

--- src/app/api/users/[username]/schedule/route.ts
+++ src/app/api/users/[username]/schedule/route.ts
@@ -1,6 +1,8 @@
-import { isBefore, startOfHour } from 'date-fns'
+import { addHours, formatISO, isBefore, startOfHour } from 'date-fns'
+import { google } from 'googleapis'
 import { type NextRequest, NextResponse } from 'next/server'
 import z from 'zod'
+import { getGoogleOAuthToken } from '@/lib/google'
 import { prisma } from '@/lib/prisma'
 
 const whitespaceRegex = /\s+/
@@ -52,13 +54,44 @@ export async function POST(
     return new NextResponse(null, { status: 400 })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       date,
       observations,
       userId: user.id,
+    },
+  })
+
+  const auth = await getGoogleOAuthToken(user.id)
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth,
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: formatISO(schedulingDate),
+      },
+      end: {
+        dateTime: formatISO(addHours(schedulingDate, 1)),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
